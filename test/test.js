ba0e26ad3592315d276ecb6cf5443bbb04cc8c3b
craig.thompson <craig.thompson@communicatorcorp.com>
2017-01-25 10:07:11 +0000

    Better output file management
    * Fixed issue with nested directories not being created
    * Better filepath management using dummy __output__ directory in test directory

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 const pkg = require('../package.json');
 const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
 const less = require('less');
 const VariablesOutput = require('../lib/index');
 const { expect } = require('chai');
@@ -15,6 +17,11 @@ const LESS = Object.keys(VARIABLES).map((v) =>
 	`@${v}: ${VARIABLES[v]};`
 )
 .join('\n');
+
+const OUTPUT_PATH = path.join(__dirname, '__output__');
+
+const qualifyPath = (...parts) => 
+	path.join(OUTPUT_PATH, ...parts);
 
 const compareVariables = (input, output) => {
 	const inputKeys = Object.keys(input);
@@ -42,7 +49,7 @@ const renderLess = (contents, opts, success, done) => {
 
 describe(pkg.name, () => {
 	it('Should output variables.json file with all top-level variables', (done) => {
-		const filename = 'variables.json';
+		const filename = qualifyPath('variables.json');
 
 		renderLess(LESS, { filename }, (output) => {
 			compareVariables(VARIABLES, output);
@@ -51,7 +58,7 @@ describe(pkg.name, () => {
 	});
 
 	it('Should output file as given filename option if provided', (done) => {
-		const filename = 'customfilename.json';
+		const filename = qualifyPath('customfilename.json');
 
 		renderLess(LESS, { filename }, (output) => {
 			compareVariables(VARIABLES, output);
@@ -59,9 +66,22 @@ describe(pkg.name, () => {
 		done);
 	});
 
+	it('Should ensure all directories in the given filename option are created', (done) => {
+		const dir1 = 'dir1';
+		const dir2 = 'dir2';
+		const name = 'directories.json';
+		const filename = qualifyPath(dir1, dir2, name);
+
+		renderLess(LESS, { filename }, (output) => {
+			expect(fs.existsSync(qualifyPath(dir1))).to.be.true;
+			expect(fs.existsSync(qualifyPath(dir1, dir2))).to.be.true;
+			expect(fs.existsSync(filename)).to.be.true;
+		},
+		done);
+	});
+
 	// Cleanup variables files
 	after(() => {
-		fs.unlinkSync('variables.json');
-		fs.unlinkSync('customFilename.json');
+		rimraf.sync(OUTPUT_PATH);
 	});
 })
